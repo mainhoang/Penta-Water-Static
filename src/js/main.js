12810bb7b7f246196af31e301982f58b6e3c39b3
elon <elon@drawingfrommemory.com>
2016-08-22 16:55:19 -0700

    animate mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,17 @@ $('.carousel').slick({
   pauseOnHover: false,
   arrows: false
 });
+
+var burger = document.querySelector('.hamburger-menu')
+var bar = document.querySelector('.bar')
+var dropin = document.querySelector('.dropin')
+var logo = document.querySelector('.master-lo')
+var partner = document.querySelector('.partner-link')
+
+burger.addEventListener('click', function () {
+  console.log('click')
+  bar.classList.toggle('animate')
+  dropin.classList.toggle('animate-menu')
+  logo.classList.toggle('hide-menu-item')
+  partner.classList.toggle('hide-menu-item')
+})
